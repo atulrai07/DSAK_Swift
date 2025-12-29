d2aa840752f1b27fd1ee2ccaab1065f34950980a
atulrai07 <atulrai.varanasi@gmail.com>
2025-12-30 02:37:40 +0530

    Initial Commit

---
+++ DSAK/DSAKApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSAKApp.swift
+//  DSAK
+//
+//  Created by Atul on 30/12/25.
+//
+
+import SwiftUI
+
+@main
+struct DSAKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
