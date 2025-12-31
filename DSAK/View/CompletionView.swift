ea1680e69eef9cd37742e47747639889162aef1b
atulrai07 <atulrai.varanasi@gmail.com>
2026-01-01 01:03:41 +0530

    Quiz and Completion Screen (3 commit)

---
+++ DSAK/View/CompletionView.swift
@@ -0,0 +1,18 @@
+//
+//  CompletionView.swift
+//  DSAK
+//
+//  Created by Atul on 30/12/25.
+//
+
+import SwiftUI
+
+struct CompletionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CompletionView()
+}
